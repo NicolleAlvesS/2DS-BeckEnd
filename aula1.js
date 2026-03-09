faca6040430151110d12b6d8f4e7967fb5542e2a
alves⋆☀︎. <nicolle.alves.silva@escola.pr.gov.br>
2026-03-09 08:54:43 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,11 @@
+let nome = "Nicolle"
+let altura = 1.60
+let idade = 16
+let pele = "branca"
+let cabelo = "castanho longo"
+let oculos = "uso óculos de grau com armação rosa"
+let uniforme = "uniforme do colégio"
+
+console.log("Olá, meu nome é  " + nome + "! possuo " + idade + " anos! minha cor de pele é " + pele + ", meu cabelo é " + cabelo + ", eu uso " + oculos + " e estou usando o " + uniforme )
+
+

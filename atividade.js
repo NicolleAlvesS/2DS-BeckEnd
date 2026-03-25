ad4c1c87bca830c92d28e82c96b9b84081acb1e4
alves⋆☀︎. <nicolle.alves.silva@escola.pr.gov.br>
2026-03-25 09:46:07 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,15 @@
+const produtos = ["Arroz", "Feijão", "Café", "Óleo de Soja", "Açúcar", "Leite", "Ovos", "Pão de forma", "Sabão em Pó", "Manteiga"]
+const precos = [   29.90 ,   8.50 ,   18.90 ,     6.29 ,       4.95 ,   4.49 ,   10.80 ,    8.99 ,         14.90 ,        10.50    ]
+
+
+let contador = 0 
+while(contador <= 10){ 
+    console.log(produtos[contador] + " - " + precos[contador])
+    contador = contador + 1
+}
+
+function novoProduto
+
+produtos.push("Creme Dental")
+
+console.log(alunos)

8eef8ab2f85a58747d84fec549357bcb6d00159e
alves⋆☀︎. <nicolle.alves.silva@escola.pr.gov.br>
2026-03-30 08:45:09 -0300

    Add files via upload

--- atividade.js
+++ atividade.js
@@ -1,15 +1,29 @@
 const produtos = ["Arroz", "Feijão", "Café", "Óleo de Soja", "Açúcar", "Leite", "Ovos", "Pão de forma", "Sabão em Pó", "Manteiga"]
 const precos = [   29.90 ,   8.50 ,   18.90 ,     6.29 ,       4.95 ,   4.49 ,   10.80 ,    8.99 ,         14.90 ,        10.50    ]
 
-
-let contador = 0 
-while(contador <= 10){ 
-    console.log(produtos[contador] + " - " + precos[contador])
-    contador = contador + 1
+function mostrarEstoque(){
+ console.log("=== Estoque ===")
+    let contador = 0 
+  while(contador < produtos.length){ 
+   console.log(produtos[contador] + " - " + precos[contador])
+   contador = contador + 1
+}
 }
 
-function novoProduto
+mostrarEstoque()
 
-produtos.push("Creme Dental")
+function adicionarEstoque(nome, preco){
+    produtos.push(nome)
+    precos.push(preco)
+    console.log("== Produto adicionado ==")
+}
+adicionarEstoque("Creme dental", 6)
+mostrarEstoque()
 
-console.log(alunos)
+function removerProduto(indice){
+   produtos.splice(indice, 1) 
+   precos.splice(indice, 1) 
+}
+
+removerProduto()
+mostrarEstoque()

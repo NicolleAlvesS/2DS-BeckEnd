07c4abc9a7338140e5063334cf5e128a738d0644
alves⋆☀︎. <nicolle.alves.silva@escola.pr.gov.br>
2026-04-29 09:44:34 -0300

    Add files via upload

---
+++ atividade2.js
@@ -0,0 +1,76 @@
+const produtos = [
+    {
+        nome: "Homem de Ferro",
+        ano: 2008, 
+        ator_principal: "Robert Downey Jr",
+        classificacao: "12 anos",
+        genero: "Ação/Ficção Científica",
+        duracao: "2h 06min"
+    },
+    {
+        nome: "Os vingadores",
+        ano: 2012,
+        ator_principal: "Robert Downey Jr", "Chris Evans", "Scarlett Johansson",
+        classificacao : "12 anos",
+        genero: "Ação/Aventura",
+        duracao :"2h 23min"
+    },
+    {
+        nome: "Capitão América: O Soldado Invernal",
+        ano: 2014,
+        ator_principal: "Chris Evans",
+        classificacao : "12 anos",
+        genero: "Ação/Suspense",
+        duracao :"2h 16min"
+    },
+    {
+        nome: "Guardiões da Galáxia",
+        ano: 2018,
+        ator_principal: "Chadwick Boseman",
+        classificacao : "14 anos",
+        genero: "Ação/Aventura",
+        duracao :"2h 14min"
+    },
+    {
+        nome: "Vingadores: Guerra Infinita",
+        ano: 2018,
+        ator_principal: "Robert Downey Jr", "Chris Hemsworth",  "Josh Brolin", 
+        classificacao : "14 anos",
+        genero: "Ação/Fantasia",
+        duracao :"2h 29min"
+    },
+    {
+
+        nome: "Vingadores: Ultimato (Endgame)",
+        ano: 2019,
+        ator_principal: "Robert Downey Jr", "Chris Hemsworth",  "Mark Ruffalo",
+        classificacao : "14 anos",
+        genero: "Ação/Sci-Fi",
+        duracao :"3h 01min"
+    },
+    {
+        nome: "Homem-Aranha: Sem Volta Para Casa",
+        ano: 2021,
+        ator_principal: "Tom Holland",
+        classificacao : "12 anos",
+        genero: "Ação/Aventura/Fantasia",
+        duracao :"2h 28min"
+    },
+    {
+        nome: "Doutor Estranho no Multiverso da Loucura",
+        ano: 2022,
+        ator_principal: "Benedict Cumberbatch",
+        classificacao : "14 anos",
+        genero: "Ação/Aventura/Fantasia",
+        duracao :"2h 06min"
+    },
+    {
+        nome: "Deadpool & Wolverine",
+        ano: 2024,
+        ator_principal: "Ryan Reynolds", "Hugh Jackman",
+        classificacao : "18 anos",
+        genero: "Ação/Comédia",
+        duracao :"2h 08min"
+    }
+
+]

521d43604d69798ffa377e581a6d820f2bea796d
alves⋆☀︎. <nicolle.alves.silva@escola.pr.gov.br>
2026-05-06 09:39:29 -0300

    Add files via upload

---
+++ atividadee2.js
@@ -0,0 +1,105 @@
+const filmes = [
+    {
+        nome: "Homem de Ferro",
+        ano: 2008, 
+        ator_principal: ["Robert Downey Jr"],
+        classificacao: "12 anos",
+        genero: "Ação/Ficção Científica",
+        duracao: "2h 06min"
+    },
+    {
+        nome: "Os vingadores",
+        ano: 2012,
+        ator_principal: ["Robert Downey Jr", "Chris Evans", "Scarlett Johansson"],
+        classificacao : "12 anos",
+        genero: "Ação/Aventura",
+        duracao :"2h 23min"
+    },
+    {
+        nome: "Capitão América: O Soldado Invernal",
+        ano: 2014,
+        ator_principal: ["Chris Evans"],
+        classificacao : "12 anos",
+        genero: "Ação/Suspense",
+        duracao :"2h 16min"
+    },
+    {
+        nome: "Thor: Ragnarok",
+        ano: 2017,
+        ator_principal: ["Chris Hemsworth"],
+        classificacao : "12 anos",
+        genero: "Ação/Aventura/Comédia",
+        duracao :"2h 10min"
+    },
+    {   nome: "Guardiões da Galáxia",
+        ano: 2018,
+        ator_principal: ["Chadwick Boseman"],
+        classificacao : "14 anos",
+        genero: "Ação/Aventura",
+        duracao :"2h 14min"
+    },
+    {
+        nome: "Vingadores: Guerra Infinita",
+        ano: 2018,
+        ator_principal: ["Robert Downey Jr", "Chris Hemsworth",  "Josh Brolin"], 
+        classificacao : "14 anos",
+        genero: "Ação/Fantasia",
+        duracao :"2h 29min"
+    },
+    {
+
+        nome: "Vingadores: Ultimato (Endgame)",
+        ano: 2019,
+        ator_principal: ["Robert Downey Jr", "Chris Hemsworth",  "Mark Ruffalo"],
+        classificacao : "14 anos",
+        genero: "Ação/Sci-Fi",
+        duracao :"3h 01min"
+    },
+    {
+        nome: "Homem-Aranha: Sem Volta Para Casa",
+        ano: 2021,
+        ator_principal: ["Tom Holland"],
+        classificacao : "12 anos",
+        genero: "Ação/Aventura/Fantasia",
+        duracao :"2h 28min"
+    },
+    {
+        nome: "Doutor Estranho no Multiverso da Loucura",
+        ano: 2022,
+        ator_principal: ["Benedict Cumberbatch"],
+        classificacao : "14 anos",
+        genero: "Ação/Aventura/Fantasia",
+        duracao :"2h 06min"
+    },
+    {
+        nome: "Deadpool & Wolverine",
+        ano: 2024,
+        ator_principal: ["Ryan Reynolds", "Hugh Jackman"],
+        classificacao : "18 anos",
+        genero: "Ação/Comédia",
+        duracao :"2h 08min"
+    }
+]
+        // Criar o JSON:
+    const filmesJSON = JSON.stringify(filmes)
+    console.log(filmesJSON)
+
+    // Criar o arquivo
+    const fs = require("fs")
+    fs.writeFileSync("filmes.json", filmesJSON)
+
+    // Ler o arquivo
+    const filmesArquivo = require("./filmes.json")
+    console.log(filmesArquivo)
+
+    // MOSTRAR OS DADOS DO ARQUIVO ORGANIZADAMENTE
+    // alunosArquivo.forEach() ... etc
+    filmesArquivo.forEach((filme)=>{
+        console.log(filme.nome + 
+            " - Ano de lançamento: " +filme.ano + 
+            " - Ator principal: " + filme.ator_principal + 
+            " - Classificação: " + filme.classificacao +
+            " - Gênero:" + filme.genero + 
+            " - Duração: " + filme.duracao) 
+                
+        })

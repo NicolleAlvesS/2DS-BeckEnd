d9c66c08ba35856ea12d3991de31843769f9adaa
alves⋆☀︎. <nicolle.alves.silva@escola.pr.gov.br>
2026-06-15 08:56:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express")
 const app = express()
-const port = 3002
+const port = 3009
 app.use(express.json())
+const fs = require('fs')
+
+
+app.post("/clientes", (req, res) =>{
+const clientes = req.body
+try { 
+    //abrir o arquivo 
+    const bd = JSON.parse(fs.readFileSync("bd.json", "utf8"))
+    //adicionar clientes 
+    bd.push(clientes)
+    //salvar o arquivo
+    fs.writeFileSync("bd.json", JSON.stringify(bd), "utf8")
+    //resposta
+    res.status(201).json({resposta: "Cliente cadastrado!"})
+} catch (erro){
+    res.status(500).json({erro: erro.message})
+}
+})
 
 
 app.get("/perfil", (req, res) =>{
